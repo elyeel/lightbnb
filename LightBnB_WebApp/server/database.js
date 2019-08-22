9b4b65687079ce240a6bdd386a900639a2905562
elyeel <elyeel@yahoo.com>
2019-08-22 23:56:23 +0000

    lightbnb reservations function added

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -94,7 +94,28 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+    SELECT
+      properties.id AS id,
+      title,
+      cost_per_night,
+      start_date,
+      end_date,
+      avg(rating) as rating,
+      cover_photo_url,
+      thumbnail_photo_url
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON property_reviews.property_id = properties.id
+    WHERE end_date < NOW()::DATE AND
+      reservations.guest_id = $1
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    limit $2;
+  `, [guest_id, limit])
+  .then(res => res.rows)
+  .catch(err => console.error('query error', err.stack));
+  // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
