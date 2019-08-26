972edfc6810d6c4e20990f97af8cbccdd7601205
Risyardi Iswanto <elyeel@yahoo.com>
2019-08-25 19:01:41 -0700

    added lightbnb all properties

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -130,36 +130,71 @@ exports.getAllReservations = getAllReservations;
 const getAllProperties = function(options, limit = 10) {
   // const limitedProperties = {};
   const queryParams = [];
+  const base = 10;
   let counter = 0;
   let queryString = `
   SELECT properties.*,
     avg(property_reviews.rating) as average_rating
   FROM properties
-  JOIN property_reviews ON properties.id = property_reviews.property_id  `;
-
-  if (options.owner_id) {
-    counter++;
-  }
-  if (options.minimum_price_per_night && options.maximum_price_per_night) {
-    counter++;
-  }
-  if 
-  switch case
-
-  if (options.owner_id) {
-    queryParams.push(`${options.owner_id}`);
-    queryString += `WHERE owner_id = $${queryParams.length}`;
-  }
+  JOIN property_reviews ON properties.id = property_reviews.property_id  
+  `;
 
   if (options.city) {
+    counter++;
     queryParams.push(`%${options.city}%`);
-    queryString += `WHERE city ILIKE $${queryParams.length}`;
+    queryString += `WHERE city ILIKE $${queryParams.length} 
+    `;
+  }  
+
+  if (options.owner_id) {
+    counter++;
+    queryParams.push(options.owner_id);
+    if (counter === 2) {
+      queryString += `AND owner_id = $${queryParams.length} 
+      `;
+      console.log('Got here!');
+    } else if (counter === 1) {
+      queryString += `WHERE owner_id = $${queryParams.length} 
+      `;
+    }
+  };
+
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    counter += 2;
+    console.log('string ?', Number.isInteger(options.maximum_price_per_night));
+    const min = parseInt(options.minimum_price_per_night, base);
+    const max = parseInt(options.maximum_price_per_night, base);
+    queryParams.push(min);
+    queryString += `AND cost_per_night >= $${queryParams.length} 
+    `;
+    queryParams.push(max);
+    queryString += `AND cost_per_night <= $${queryParams.length} 
+    `;
+  };
+
+  queryString += `GROUP BY properties.id
+  `;
+
+  if (options.minimum_rating) {
+    counter ++;
+    queryParams.push(options.minimum_rating);
+    queryString += `HAVING AVG(property_reviews.rating) >= $${queryParams.length} 
+    `;
   }
+
+
+  // if 
+  // switch case
+
+  // if (options.owner_id) {
+  //   queryParams.push(`${options.owner_id}`);
+  //   queryString += `WHERE owner_id = $${queryParams.length}`;
+  // }
+
+
   
   queryParams.push(limit);
-  queryString += `
-  GROUP BY properties.id
-  ORDER BY cost_per_night
+  queryString += `ORDER BY cost_per_night
   LIMIT $${queryParams.length};
   `;
 
