f745b56c59939835bc7a3fce0b774e7cb961497f
Risyardi Iswanto <elyeel@yahoo.com>
2020-08-25 00:22:47 -0700

    testing with nvm v10, found unable to log in error

--- 1_queries/all_my_reservations.sql
+++ 1_queries/all_my_reservations.sql
@@ -6,7 +6,7 @@ SELECT
   avg(rating)
 FROM reservations
 JOIN properties ON reservations.property_id = properties.id
-JOIN property_reviews ON property_reviews.property_id = properties.id
+JOIN property_reviews ON properties.id = property_reviews.property_id
 WHERE end_date < NOW()::DATE AND
   reservations.guest_id = 1
 GROUP BY properties.id, reservations.id
--- 1_queries/most_visited_cities.sql
+++ 1_queries/most_visited_cities.sql
@@ -2,6 +2,6 @@ SELECT
   city,
   count(reservations.id) as total_reservations
 FROM reservations
-JOIN properties ON properties.id = property_id
-GROUP BY city
+JOIN properties ON property_id = properties.id
+GROUP BY properties.city
 ORDER BY total_reservations DESC;
